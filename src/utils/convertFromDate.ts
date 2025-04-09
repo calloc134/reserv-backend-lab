db387b267fa0e510b5c1ea42e290e9a4acb35c3c
calloc134 <115209243+calloc134@users.noreply.github.com>
2025-04-09 11:15:42 +0900

    日時の表示を修正

--- src/utils/convertFromDate.ts
+++ src/utils/convertFromDate.ts
@@ -2,5 +2,7 @@ export function convertFromDate(date: Date): string {
 	const year = date.getFullYear();
 	const month = date.getMonth() + 1;
 	const day = date.getDate();
-	return `${year}-${month}-${day}`;
+	// yyyy-MM-dd形式に変換
+	// return `${year}-${month}-${day}`;
+	return `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
 }
